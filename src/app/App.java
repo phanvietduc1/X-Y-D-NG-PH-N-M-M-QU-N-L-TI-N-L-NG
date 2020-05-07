28272f904a503daf2b3de0da9b15545eee271871
vietDuc <phanvietduc1@gmail.com>
2020-05-07 22:31:12 +0700

    Giao dien cua so login

---
+++ src/app/App.java
@@ -0,0 +1,9 @@
+package app;
+
+public class App{
+    public static void main(String[] args) throws Exception {
+        // new App() . setVisible(true);
+        GUI app = new GUI("title");
+        app.doShow();
+    }
+}
